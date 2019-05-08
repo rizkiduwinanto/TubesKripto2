3bae5eb45b1ae0f1e9da2925af57f5850d32858e
Yora <13516067@std.stei.itb.ac.id>
2019-05-08 21:53:31 +0700

    edit read email

--- api/readEmail.py
+++ api/readEmail.py
@@ -1,14 +1,22 @@
 import easyimap
+import json
 
 login = input("input email address : ")
 password = input("input password : ")
 
+mail_inbox = {}
 imapper = easyimap.connect('imap.gmail.com', login, password)
-for mail_id in imapper.listids(limit=100):
+i=0
+for mail_id in imapper.listids(limit=5):
     mail = imapper.mail(mail_id)
-    print(mail.from_addr)
-    print(mail.to)
-    print(mail.cc)
-    print(mail.title)
-    print(mail.body)
-    print(mail.attachments)
+    inbox = {}
+    inbox['from'] = mail.from_addr
+    inbox['to'] = mail.to
+    inbox['cc'] = mail.cc
+    inbox['title'] = mail.title
+    inbox['body'] = mail.body
+    mail_inbox[i]=inbox
+    i+=1
+
+json_string = json.dumps(mail_inbox)
+print(json_string)
